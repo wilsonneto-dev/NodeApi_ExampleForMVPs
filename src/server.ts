8cfb4832023ca40df45d8645ef86a7f2a7a309f2
Wilson Neto <contato@wilsonneto.com.br>
2020-07-11 14:32:14 -0300

    feat(middlewares: celebrate): adicionando o middleware do celebrate para lidar com erros que ele próprio gerar

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import "reflect-metadata";
-
 import express from "express";
 import "express-async-errors";
 
+import { errors as celebrateErrors } from "celebrate";
 import errorHandler from "./middlewares/errorHandler";
 
 import routes from "./routes";
@@ -21,6 +21,7 @@ app.use("/files", express.static(uploadConfig.directory));
 
 app.use(routes);
 
+app.use(celebrateErrors());
 app.use(errorHandler);
 
 app.listen(3333, () => {
