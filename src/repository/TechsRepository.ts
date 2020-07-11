b642169afef7e6b69a520174e158a553c93e932b
Wilson Neto <contato@wilsonneto.com.br>
2020-07-11 11:22:25 -0300

    feature(repositories): criado o TechRepository

---
+++ src/repository/TechsRepository.ts
@@ -0,0 +1,7 @@
+import { EntityRepository, Repository } from "typeorm";
+import Tech from "models/Tech";
+
+@EntityRepository(Tech)
+class TechsRepository extends Repository<Tech> {}
+
+export default TechsRepository;
